0d028f8f3f365192cd1884ed5d1799dc90cb77f0
20Singh <kritikasingh2315@gmail.com>
2022-05-28 16:35:45 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Route, Routes} from "react-router-dom"
+import Home from "./Component/Home"
+import Bollywood from './Component/Bollywood';
+import Technology from './Component/Technology';
+import Food from './Component/Food';
+import Fitness from './Component/Fitness';
+import Hollywood from './Component/Hollywood';
+import Homebox from './Pages/Homebox';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+      <div className="App">
+        <BrowserRouter>
+          <Home />
+          <Routes>
+            <Route path="/" element= {<Homebox />} />
+            <Route path="/Homebox" element= {<Homebox />} />
+            <Route path="/bollywood" element={<Bollywood />} />
+            <Route path="/technology" element={<Technology />} />
+            <Route path="/hollywood" element={<Hollywood />} />
+            <Route path="/fitness" element={<Fitness />} />
+            <Route path="/food" element={<Food />} />
+
+          </Routes>
+        </BrowserRouter>
+      </div>
   );
 }
 
